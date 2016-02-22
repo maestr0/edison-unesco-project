f0076d3cfb49845a05ac94872c680c81ed075e69
Pawel Raszewski <pawel.raszewski@teamaol.com>
2016-02-21 20:30:22 -0500

    better status

--- downloader-www/routes/index.js
+++ downloader-www/routes/index.js
@@ -6,11 +6,11 @@ var exec = require('child_process').exec;
 /* GET home page. */
 router.get('/', function (req, res, next) {
     var status = "ss";
-    exec("hostnamectl", function (error, stdout, stderr) {
+    exec("hostnamectl ; echo -e \"\n\n\" && df -h && echo -e \"\n\n\" && free", function (error, stdout, stderr) {
         if (!error) {
             status = stdout;
         } else {
-            status = "Error: " + stderr + error;
+            status = stderr + "\n\n" + error;
         }
 
         res.render('index', {title: 'UNESCO monitoring station', status: status});
