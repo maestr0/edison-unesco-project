559280b1f6c10b2f1bedf0b98caa0d7d0d497555
Pawel Raszewski <pawel.raszewski@teamaol.com>
2016-02-21 20:09:49 -0500

    added a simple nodejs app to download images from Edison

---
+++ downloader-www/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+var exec = require('child_process').exec;
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    var status = "";
+    exec("hostnamectl", function (error, stdout, stderr) {
+        if (!error) {
+            status = stdout;
+        } else {
+            status = "Error: " + stderr + error;
+        }
+    });
+
+    res.render('index', {title: 'UNESCO monitoring station', status: status});
+});
+
+module.exports = router;
