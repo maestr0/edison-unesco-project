8dd26d6ef39e9c8620c18a60692db7ba4a60a024
Pawel Raszewski <pawel.raszewski@teamaol.com>
2016-02-21 22:09:47 -0500

    UNICEF

--- downloader-www/routes/index.js
+++ downloader-www/routes/index.js
@@ -13,7 +13,7 @@ router.get('/', function (req, res, next) {
             status = stderr + "\n\n" + error;
         }
 
-        res.render('index', {title: 'UNESCO monitoring station', status: status});
+        res.render('index', {title: 'UNICEF monitoring station', status: status});
     });
 });
 
