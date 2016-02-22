b1426ebbdde00c07c88779d3440b65b6de8221a2
Pawel Raszewski <pawel.raszewski@teamaol.com>
2016-02-21 22:22:34 -0500

    better status report

--- downloader-www/routes/index.js
+++ downloader-www/routes/index.js
@@ -5,12 +5,22 @@ var exec = require('child_process').exec;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var status = "ss";
-    exec("hostnamectl ; echo -e \"\n\n\" && df -h && echo -e \"\n\n\" && free", function (error, stdout, stderr) {
+    var status = "";
+    var commands = [
+        "date",
+        "configure_edison --showWiFiIP",
+        "configure_edison --showWiFiMode",
+        "configure_edison --showNames",
+        "hostnamectl",
+        "df -h",
+        "free -h"
+    ].join('; echo "\n\n" && ');
+
+    exec(commands, function (error, stdout, stderr) {
         if (!error) {
             status = stdout;
         } else {
-            status = stderr + "\n\n" + error;
+            status = stdout + "\n\n" + stderr + "\n\n" + error;
         }
 
         res.render('index', {title: 'UNICEF monitoring station', status: status});
