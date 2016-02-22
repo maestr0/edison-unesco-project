c5d283854bb2d5c3f61d5cf09281c0869cf1c3a3
Pawel Raszewski <pawel.raszewski@teamaol.com>
2016-02-21 20:20:08 -0500

    fixed status

--- downloader-www/routes/index.js
+++ downloader-www/routes/index.js
@@ -5,16 +5,16 @@ var exec = require('child_process').exec;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    var status = "";
+    var status = "ss";
     exec("hostnamectl", function (error, stdout, stderr) {
         if (!error) {
             status = stdout;
         } else {
             status = "Error: " + stderr + error;
         }
-    });
 
-    res.render('index', {title: 'UNESCO monitoring station', status: status});
+        res.render('index', {title: 'UNESCO monitoring station', status: status});
+    });
 });
 
 module.exports = router;
